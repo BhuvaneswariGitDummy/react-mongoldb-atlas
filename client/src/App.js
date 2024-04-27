5c44473bbff4a98b2e9ccc8c2eb7859459776e6e
BhuvaneswariGitDummy <dummygit00@gmail.com>
2024-04-27 12:10:26 +0530

    first

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Read from './components/Read';
+import Write from './components/Write';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/read' element={<Read />} />
+          <Route path='/write' element={<Write />} />
+          <Route path='/' element={<Write />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Read.js
@@ -0,0 +1,25 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const Read = () =>{
+    const [serverData, setServerData] = useState()
+    useEffect(()=>{
+        const fetchData = async() =>{
+            try{
+                const response = await axios.get('http://localhost:8080/readfromserver')
+                const dataFromServer = response.data.message
+                setServerData(dataFromServer)
+            }
+            catch(error){
+                console.log('Error fetching the data',error.message)
+            }
+        }
+        fetchData()
+    },[])
+    return(
+        <div>
+            {serverData}
+        </div>
+    )
+}
+export default Read;
